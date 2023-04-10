9be0b5079eb73cd2229d082859f10b95c5fd0a9d
Mike.Goy <Mike.Goy@RAINBOWKITTEN>
2023-04-10 14:04:44 -0700

    Template

--- Lab 10/Lab 10.cpp
+++ Lab 10/Lab 10.cpp
@@ -2,18 +2,43 @@
 //
 
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Piece {
+    char sprite = '\0';
+    int x;
+    int y;
+};
 
 int main()
 {
-   
+    // input file
+    ifstream gameFile;
+    gameFile.open("C:\\Users\\mike.goy\\Documents\\persons.txt");
+    if (gameFile.fail() || !gameFile.is_open()) {
+        cout << "Uh oh, we can't read the file" << endl;
+        exit(1);
+    }
 
+    // fix vector to hold person info
+    Piece pieces[10];
+    Piece current;
+    int i = 0;
+    while (gameFile >> current.sprite >> current.x >> current.y) {
+        pieces[i] = current;
+        i++;
+    }
 
+    // display pieces
+    for (Piece& p : pieces) {
+        if (p.sprite == '\0') {
+            break;
+        }
+        cout << p.sprite << p.x << p.y << endl;
+    }
 
-
-
-
-
-
+    return 0;
 
 }
-
